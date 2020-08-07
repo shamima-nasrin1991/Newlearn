18ad4ab2c3a9f1c5439e9100f154e141ff9a54b3
[shamima] <[shamimas102@gmail.com]>
2020-08-06 20:43:26 -0400

    new modify

--- src/practise_h/Learn.java
+++ src/practise_h/Learn.java
@@ -1,8 +1,24 @@
 package practise_h;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Learn {
 
 	public static void main(String[] args) {
+		/*System.setProperty("webdriver.chrome.driver","C://Users//Imtiaz//eclipse-workspace//Selenium_home_p1//DRIVER//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();*/
+		System.setProperty("webdriver.gecko.driver","C://Users//Imtiaz//eclipse-workspace//Selenium_home_p1//DRIVER//geckodriver.exe ");
+		WebDriver driver= new FirefoxDriver();
+		//using implicit wait for page loading
+		driver.manage().timeouts().implicitlyWait(50,TimeUnit.SECONDS);
+		driver.get("https://www.ebay.com/");
+		System.out.println("Webpage open successfully");
+		driver.findElement(By.id("")).click();
+		//here need something
 		// TODO Auto-generated method stub
 
 	}
